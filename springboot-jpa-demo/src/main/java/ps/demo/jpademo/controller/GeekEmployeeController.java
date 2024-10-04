389e8825320a83a53fb344c21d0def453d776859
yp <yysyp@foxmail.com>
2024-10-04 22:21:37 +0800

    add validation

---
+++ springboot-jpa-demo/src/main/java/ps/demo/jpademo/controller/GeekEmployeeController.java
@@ -0,0 +1,22 @@
+package ps.demo.jpademo.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ps.demo.jpademo.dto.GeekEmployee;
+
+@RestController
+public class GeekEmployeeController {
+
+    @PostMapping("/geekemployees")
+    public ResponseEntity<GeekEmployee> saveEmployeeData(
+            @Valid @RequestBody GeekEmployee geekEmployee) {
+
+        return new ResponseEntity<GeekEmployee>(
+                geekEmployee, HttpStatus.CREATED);
+    }
+
+}
