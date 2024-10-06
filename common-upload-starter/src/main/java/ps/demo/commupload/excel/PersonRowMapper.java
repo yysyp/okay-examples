17023f856098663c15cb227e5cb9098b7a7f0780
yp <yysyp@foxmail.com>
2024-10-06 22:47:01 +0800

    add index check

--- common-upload-starter/src/main/java/ps/demo/commupload/excel/PersonRowMapper.java
+++ common-upload-starter/src/main/java/ps/demo/commupload/excel/PersonRowMapper.java
@@ -1,14 +1,21 @@
 package ps.demo.commupload.excel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.extensions.excel.RowMapper;
 import org.springframework.batch.extensions.excel.support.rowset.RowSet;
 import ps.demo.commupload.batchjob.model.Person;
 
+@Slf4j
 public class PersonRowMapper implements RowMapper<Person> {
 
     @Override
     public Person mapRow(RowSet rowSet) throws Exception {
         Person person = new Person();
+        String[] columns = rowSet.getMetaData().getColumnNames();
+        int i = 0;
+        for (String column : columns) {
+            log.info("column ["+(i++)+"] " + column);
+        }
         person.setFirstName(rowSet.getProperties().getProperty("First Name"));
         person.setLastName(rowSet.getProperties().getProperty("Last Name"));
         person.setAge(Integer.valueOf(rowSet.getProperties().getProperty("Age")));
