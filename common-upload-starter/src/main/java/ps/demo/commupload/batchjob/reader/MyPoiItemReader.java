60cffaf2754d3c0b19c4c02d885eadcaf767455e
yp <yysyp@foxmail.com>
2024-10-14 10:59:04 +0800

    add openExcelFile overwrite

--- common-upload-starter/src/main/java/ps/demo/commupload/batchjob/reader/MyPoiItemReader.java
+++ common-upload-starter/src/main/java/ps/demo/commupload/batchjob/reader/MyPoiItemReader.java
@@ -25,14 +25,24 @@ public class MyPoiItemReader<T> extends PoiItemReader<T> {
             public void handleRow(RowSet rowSet) {
                 String[] rowData = rowSet.getCurrentRow();
                 headerRowSets.add(Arrays.copyOf(rowData, rowData.length));
-                int count = getNumberOfSheets();
-                for (int i = 0; i < count; i++) {
-                    sheetNames.add(getSheet(i).getName());
-                }
-                log.info("sheetNames={} rowSet at{} value{}", sheetNames, rowSet.getCurrentRowIndex(), rowSet);
+//                int count = getNumberOfSheets();
+//                for (int i = 0; i < count; i++) {
+//                    sheetNames.add(getSheet(i).getName());
+//                }
+                log.info("rowSet at{} value{}", rowSet.getCurrentRowIndex(), rowSet);
             }
         });
     }
 
+
+    @Override
+    protected void openExcelFile(Resource resource, String password) throws Exception {
+        super.openExcelFile(resource, password);
+        int count = getNumberOfSheets();
+        for (int i = 0; i < count; i++) {
+            sheetNames.add(getSheet(i).getName());
+        }
+        log.info("sheetNames={}", sheetNames);
+    }
 
 }
